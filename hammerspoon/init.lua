5733f6994cc160a000c9d29aed1f579cc95cc727
Phil <philipcatterall@gmail.com>
2024-09-18 21:16:57 -0400

    hammerspoon updates

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,11 +9,23 @@ function reloadConfig(files)
         hs.reload()
     end
 end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.dotfiles/hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
 --------------------------------------
--- Recursive Keybindings            --
+----------- Countdown ----------------
+--------------------------------------
+
+countDown=hs.loadSpoon("CountDown")
+countDown.defaultDuration = 25
+countDown.menuBarAlwaysShow = true
+countDown.warningFormat = "%02d minutes remaining"
+countDown.menuBarIconActive = "⏰"
+countDown.menuBarIconIdle = "⏲️"
+
+
+--------------------------------------
+-------- Recursive Keybindings -------
 --------------------------------------
 hs.loadSpoon("RecursiveBinder")
 spoon.RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
@@ -24,14 +36,57 @@ function openDirectory(path)
     hs.execute(shell_command)
 end
 
-local keyMap = {
-    [singleKey('f', 'finder+')] = {
-        [singleKey('a', 'applications')] = function() openDirectory('~/Applications') end,
-        [singleKey('d', 'downloads')] = function() openDirectory('~/Downloads') end,
-        [singleKey('t', 'tmp')] = function() openDirectory('~/tmp') end
-
+if hs.host.localizedName() == 'ActionIQ-philipcatterall' then
+    keyMap = {
+        [singleKey('f', 'finder+')] = {
+            [singleKey('a', 'applications')] = function() openDirectory('~/Applications') end,
+            [singleKey('d', 'downloads')] = function() openDirectory('~/Downloads') end,
+            [singleKey('t', 'tmp')] = function() openDirectory('~/tmp') end
+        },
+        [singleKey('b', 'bookmarks+')] = {
+            [singleKey('g', 'github+')] = {
+                [singleKey('a', 'aiq')] = function() hs.urlevent.openURL('https://www.github.com/ActionIQ/aiq') end,
+                [singleKey('d', 'datascience')] = function() hs.urlevent.openURL('https://github.com/ActionIQ/datascience/tree/master/databricks-analytics-pipelines') end,
+                [singleKey('s', 'sandbox')] = function() hs.urlevent.openURL('https://www.github.com/ActionIQ/sandbox/tree/master/user/phil') end
+            },
+            [singleKey('j', 'jira+')] = {
+                [singleKey('e', 'dpe')] = function() hs.urlevent.openURL('https://actioniq.atlassian.net/jira/software/projects/DP/boards/242/backlog?epics=visible') end,
+                [singleKey('d', 'design')] = function() hs.urlevent.openURL('https://actioniq.atlassian.net/jira/software/projects/PD/boards/237') end,
+                [singleKey('t', 'docs')] = function() hs.urlevent.openURL('https://actioniq.atlassian.net/jira/software/projects/DOC/boards/214') end,
+            }
+        },
+        [singleKey('t', 'timer+')] = {
+            [singleKey('s', 'start')] = function() countDown:startFor(25) end,
+            [singleKey('p', 'pause/resume')] = function() countDown:pauseOrResume() end,
+            [singleKey('c', 'cancel')] = function() countDown:cancel() end
+        },
+        [singleKey('n', 'nightshift')] = function() hs.execute('shortcuts run "Toggle Night Shift"') end
     }
-}
+elseif hs.host.localizedName() == "Philip's MacBook Air" then
+    keyMap = {
+        [singleKey('f', 'finder+')] = {
+            [singleKey('a', 'applications')] = function() openDirectory('~/Applications') end,
+            [singleKey('d', 'downloads')] = function() openDirectory('~/Downloads') end,
+            [singleKey('t', 'tmp')] = function() openDirectory('~/tmp') end
+        },
+        [singleKey('b', 'bookmarks+')] = {
+            [singleKey('g', 'gmail')] = function() hs.urlevent.openURL('https://gmail.com') end,
+            [singleKey('c', 'cal')] = function() hs.urlevent.openURL('https://calendar.google.com') end
+        },
+        [singleKey('t', 'timer+')] = {
+            [singleKey('s', 'start')] = function() countDown:startFor(25) end,
+            [singleKey('p', 'pause/resume')] = function() countDown:pauseOrResume() end,
+            [singleKey('c', 'cancel')] = function() countDown:cancel() end
+        },
+        [singleKey('n', 'nightshift')] = function() hs.execute('shortcuts run "Toggle Night Shift"') end
+    }
+else
+    keyMap = {}
+    hs.alert.show('Host not recognized')
+end
 
 hs.hotkey.bind({'option'}, 'space', spoon.RecursiveBinder.recursiveBind(keyMap))
+
+------------------------------------
+
 
